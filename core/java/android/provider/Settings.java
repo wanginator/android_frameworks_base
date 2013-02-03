084b08afbd5571b7379985d05a146d0dee46ad12
DvTonder <david.vantonder@gmail.com>
2013-02-03 14:28:03 -0500

    Audio: make safe headset volume user configurable

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2485,6 +2485,12 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
 
         /**
+         * Whether to prevent loud volume levels when headset is first plugged in.
+         * @hide
+         */
+        public static final String SAFE_HEADSET_VOLUME = "safe_headset_volume";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
