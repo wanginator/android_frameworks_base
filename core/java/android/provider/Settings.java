a5ea5a1c45bcddbac5fd207164dbfc492567a756
Danny Baumann <dannybaumann@web.de>
2013-11-09 13:10:15 +0100

    Quick launch shortcuts

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2589,6 +2589,17 @@ public final class Settings {
         public static final String STATUS_BAR_NATIVE_BATTERY_PERCENTAGE = "status_bar_native_battery_percentage";
 
         /**
+         * Custom navring actions
+         *
+         * @hide
+         */
+        public static final String[] NAVIGATION_RING_TARGETS = new String[] {
+            "navigation_ring_targets_0",
+            "navigation_ring_targets_1",
+            "navigation_ring_targets_2",
+        };
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
