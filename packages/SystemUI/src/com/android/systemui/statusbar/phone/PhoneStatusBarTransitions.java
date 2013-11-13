c31e532d87a3b277449f507b61601886b43d2a31
Lars Greiss <kufikugel@googlemail.com>
2013-11-13 09:10:21 +0100

    fb: add Battery Styles the new way 1/2
    google draws now the battery icon which fits perfectly into our
    concept. Beside that the batteryview is as well used on QS.
    So we readd our battery icons (icon, percentage, icon with percentage
    circle, circle perc, dot circle, dot circle perc) back and all are
    drawn. As well the icon does not only change on statusbar as well
    on QS it is reflected.
    
    As usual color options are present for the User to style the icon
    
    Change-Id: Iae9021af56a21b8d39efc5162186eb17ecc933e2
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/res/values/mahdi_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock;
+    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock,mNetworkTraffic;
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
