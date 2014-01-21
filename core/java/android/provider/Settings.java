94e5a6144a53f7e2b96bb7ae9898d6b51746389c
snak3ater <saeedyasir91@gmail.com>
2014-01-21 21:45:18 +0500

    removed unused stuff

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2501,11 +2501,6 @@ public final class Settings {
          * Nonzero values indicate that the user has a bukkit.
          * Backward-compatible with <code>PrefGetPreference(prefAllowEasterEggs)</code>.
 
-         * Whether power menu screen record is enabled
-         * @hide
-         */
-        public static final String POWER_MENU_SCREENRECORD_ENABLED = "power_menu_screenrecord_enabled";
-
         /**
          * Enable Stylus Gestures
          *
@@ -2526,11 +2521,11 @@ public final class Settings {
          */
         public static final String POWER_MENU_SCREENRECORD_ENABLED = "power_menu_screenrecord_enabled";
 
-        /**
-         * Enable Stylus Gestures
-         *
-         * @hide
-         */
+/**
+* Boolean value whether to link ringtone and notification volumes
+* @hide
+*/
+
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
@@ -2649,23 +2644,6 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
-        /**
-         * Width and height of output vide expressed in WxH
-         * @hide
-         */
-        public static final String SCREEN_RECORDER_OUTPUT_DIMENSIONS = "screen_recorder_output_dimensions";
-
-        /**
-         * Screen recorder framerate in bits per second
-         * @hide
-         */
-        public static final String SCREEN_RECORDER_BITRATE = "screen_recorder_bitrate";
-
-        /**
-         * Whether to include audio when recording a video
-         * @hide
-         */
-        public static final String SCREEN_RECORDER_RECORD_AUDIO = "screen_recorder_record_audio";
 
         /**
          * Width and height of output vide expressed in WxH
