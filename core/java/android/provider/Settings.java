c07b8390a6a2da54d4204da94171affa5693d295
AndroidRul3z <ferrettidario@gmail.com>
2014-02-07 13:27:37 +0100

    Lockscreen: Enable-Disable power menu ported from omnirom
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2679,6 +2679,12 @@ public final class Settings {
          */
         public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
 
+         /**
+         * This preference enables showing the power menu on LockScreen.
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
