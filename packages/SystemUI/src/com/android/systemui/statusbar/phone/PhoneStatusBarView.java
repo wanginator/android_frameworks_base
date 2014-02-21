3c7b8b194495c495a01a6ebb70f7c494649c960f
Lars Greiss <kufikugel@googlemail.com>
2014-02-21 22:46:31 +0100

    fb: Add Wifi name to notification drawer when Wifi is connected
    compared to 4.3 we do not make this optional any more. If it is connected
    it will show up in the notification drawer permanently and behaves same
    like the carrier label
    
    Change-Id: I058b6de4e546b4fa9b9cfef6c765664e135de2e2
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -269,6 +269,6 @@ public class PhoneStatusBarView extends PanelBar {
 
         mBar.animateHeadsUp(mNotificationPanel == panel, mPanelExpandedFractionSum);
 
-        mBar.updateCarrierLabelVisibility(false);
+        mBar.updateCarrierAndWifiLabelVisibility(false);
     }
 }
