8916d26ca371b11f9e19e2c55f6a71c630a5790e
snak3ater <saeedyasir91@gmail.com>
2014-03-20 20:06:20 +0500

    Frameworks: Wakelock Blocker (1/2)
    Change-Id: I38114529e535a3168ddb2135b10d7811347d7cf2
    Signed-off-by: Arnav Gosain <nolinuxnoparty@gmail.com>
    
    Conflicts:
    	core/java/android/os/IPowerManager.aidl
    	core/java/android/provider/Settings.java
    	services/java/com/android/server/power/PowerManagerService.java
    
    Conflicts:
    	services/java/com/android/server/power/PowerManagerService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2623,6 +2623,18 @@ public final class Settings {
          */
         public static final String NAV_BUTTONS = "nav_buttons";
 
+	/**
+	* Enable blocking wakelock
+	* @hide
+	*/
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+	* List of wakelock blocks selected
+	* @hide
+	*/
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
         /**
          * Whether incomming call UI stays in background
          *
